c1dc55ed412c7ace4d34831cb5f7e948cb23e11c
Drew <drew@Drews-MacBook-Pro.local>
2025-08-05 07:12:46 -0600

    3

--- 136-interfaces-polymorph/main.go
+++ 136-interfaces-polymorph/main.go
@@ -11,6 +11,11 @@ type secretAgent struct {
 	ltk bool
 }
 
+type farmer struct {
+	person
+	tools int
+}
+
 func (p person) speak() {
 	fmt.Println("I am", p.first)
 }
@@ -19,11 +24,24 @@ func (sa secretAgent) speak() {
 	fmt.Println("I am a secret agent", sa.first)
 }
 
+func (f farmer) accent() {
+	fmt.Println("Y'ALL, ma name is", f.first)
+}
+
 type human interface {
 	speak()
 }
 
+type accent interface { // <--
+	accent()
+}
+
 func saySomething(h human) {
+	h.speak()
+}
+
+func downSouth(a accent, h human) { // I made another type interface to see if I could double up on types and it worked
+	a.accent()
 	h.speak()
 }
 
@@ -37,9 +55,15 @@ func main() {
 		first: "Jenny",
 	}
 
+	f1 := farmer{
+		person: person{first: "Jimmy"},
+		tools:  4,
+	}
+
 	//sa1.speak()
 	//p2.speak()
 
 	saySomething(sa1)
 	saySomething(p2)
+	downSouth(f1, f1)
 }
