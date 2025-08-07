00bf326f39dc546164d240c270d3e291e20b589a
Drew <drew@Drews-MacBook-Pro.local>
2025-08-07 07:11:38 -0600

    8

--- 144-returning-a-func/main.go
+++ 144-returning-a-func/main.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import "fmt"
@@ -16,4 +17,25 @@ func bar() func() int { //This is a function returning a function because functi
 	return func() int {
 		return 42
 	}
+}
+*/
+
+package main
+
+import "fmt"
+
+// Passing 'add' function as argument
+// This function returns another function
+func incrementer() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+func main() {
+	increment := incrementer()
+	fmt.Println(increment()) // Output: 1
+	fmt.Println(increment()) // Output: 2
+	fmt.Println(increment()) // Output: 3
 }
