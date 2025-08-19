7d36b8c5b7351f263db50d32e762ec63337cb2f4
Drew <drew@Drews-MacBook-Pro.local>
2025-08-19 06:39:29 -0600

    25

---
+++ 204-ninja-9.5/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+}
